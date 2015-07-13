ce2dc7ae0ed3434b9cd11c25b913735a38ef0037
Chris Eidsmoe <ceidsmoe@uchicago.edu>
2015-07-13 15:22:26 -0500

    readjusted clear color for MinVR

--- spatialize/source/example/ExampleCube.cpp
+++ spatialize/source/example/ExampleCube.cpp
@@ -189,7 +189,7 @@ const Box& ExampleCube::getBoundingBox() {
 void Spatialize::ExampleCube::draw(float time, MinVR::CameraRef camera,
 		MinVR::WindowRef window, Shader shader) {
 
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	shader.Use();    
